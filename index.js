f97ceb337c62d6d04fea944282afe6f5e187bd26
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-04-24 19:52:07 +0200

    Add files via upload
    All done

---
+++ index.js
@@ -0,0 +1,22 @@
+var firstDiceNumber = Math.ceil(Math.random() * 6);
+var secondDiceNumber = Math.ceil(Math.random() * 6);
+
+document.querySelector(".dice .img1").setAttribute("src", "images/dice" + firstDiceNumber + ".png");
+document.querySelector(".dice .img1").setAttribute("value1", firstDiceNumber);
+document.querySelector(".dice .img2").setAttribute("src", "images/dice" + secondDiceNumber + ".png");
+document.querySelector(".dice .img2").setAttribute("value2", secondDiceNumber);
+
+if (firstDiceNumber > secondDiceNumber) {
+    document.getElementById("score").textContent = "🚩Player 1 wins!";
+}
+else if(secondDiceNumber>firstDiceNumber)
+ {
+    document.getElementById("score").textContent = "Player 2 wins!🚩";
+}
+else
+{
+    document.getElementById("score").textContent = "It's a draw!";
+}
+
+
+
